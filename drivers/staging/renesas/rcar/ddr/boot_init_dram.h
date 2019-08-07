a7b8d7f5c6e0cbd3d38a52f7381935f66447d8b1
Marek Vasut <marek.vasut+renesas@gmail.com>
2019-08-07 18:05:05 +0200

    rcar_get3: drivers: ddr: Clean up common code
    Do minor coding style changes to the common DDR init code to make it
    checkpatch compliant and move macros out into rcar_def.h.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Change-Id: I67eadf8099e4ff8702105c9e07b13f308d9dbe3d

--- drivers/staging/renesas/rcar/ddr/boot_init_dram.h
+++ drivers/staging/renesas/rcar/ddr/boot_init_dram.h
@@ -9,10 +9,10 @@
 
 extern int32_t rcar_dram_init(void);
 
-#define INITDRAM_OK (0)
-#define INITDRAM_NG (0xffffffff)
-#define INITDRAM_ERR_I (0xffffffff)
-#define INITDRAM_ERR_O (0xfffffffe)
-#define INITDRAM_ERR_T (0xfffffff0)
+#define INITDRAM_OK		0
+#define INITDRAM_NG		0xffffffff
+#define INITDRAM_ERR_I		0xffffffff
+#define INITDRAM_ERR_O		0xfffffffe
+#define INITDRAM_ERR_T		0xfffffff0
 
 #endif /* BOOT_INIT_DRAM_H */
